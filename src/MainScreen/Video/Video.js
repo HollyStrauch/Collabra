c973eeea3fc77e50c704c97b1c9cabcbc1710ac0
Holly Strauch <hollystrauch14@gmail.com>
2020-05-20 12:42:18 -0700

    Preliminary Video Display

--- src/MainScreen/Video/Video.js
+++ src/MainScreen/Video/Video.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Video.css' 
+import icon from '../../call.png'
 
 export default class Video extends React.Component {
     render() {
         return (
             <div className="Video-header">
-                Hello, I am Video Box
+                <div className="Video-iconDisplay">
+                    <img className="Video-icon" src={icon} />
+                </div>
+                <div className="Video-display">
+                    <p>In Call With:</p>
+                </div>
             </div>
         );
     }
