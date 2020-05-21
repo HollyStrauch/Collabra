8dd61e9bef29946cdaf0a6666b6abd857b12059e
Holly Strauch <hollystrauch14@gmail.com>
2020-05-20 21:01:09 -0700

    Change and resize video image

--- src/MainScreen/Video/Video.js
+++ src/MainScreen/Video/Video.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Video.css' 
-import icon from '../../call.png'
+import icon from '../../person.jpg'
 
 export default class Video extends React.Component {
     render() {
         return (
             <div className="Video-header">
                 <div className="Video-iconDisplay">
+                    <img className="Video-icon" src={icon} />
                     <img className="Video-icon" src={icon} />
                 </div>
                 <div className="Video-display">
