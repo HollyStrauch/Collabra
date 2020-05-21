ed33579a8e55d09d1df8b3b53669ffd7ad326b7c
Rick Patneaude <Rick.Patneaude@gmail.com>
2020-05-21 03:00:41 -0700

    Modal buttons and friends starter

--- src/SideMenu/SideMenu.js
+++ src/SideMenu/SideMenu.js
@@ -6,23 +6,57 @@ http://reactcommunity.org/react-modal/
 import React from 'react';
 import './SideMenu.css';
 import ReactModal from 'react-modal';
+import  Button from 'react-bootstrap/button';
+import {Accordion, Container} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
 
 export default class SideMenu extends React.Component {
     constructor(props) {
         super(props);
         /* change this to true to make modal appear*/
         this.state = {modal1Open: false};
+        this.state = {modal2Open: false};
       }
 
     render() {
         return (
             <div>
                 <div className="SideMenu-header">
-                    Hello, I am a side menu
+                    <div>
+                    <Container>
+                    <Button  variant="primary" size="lg" onClick={()=>{this.setState(state => ({modal1Open: true}))}}>
+                            Notifications
+                    </Button>
+                    </Container>
+                    </div>
+                    <div>
+                        <Accordion>
+                            <Card>
+                                <Card.Header>
+                                    <Accordion.Toggle as={Button} variant="primary" eventKey="0">
+                                        Friends
+                                    </Accordion.Toggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="0">
+                                    <Card.Body>Hello! I'm the body</Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>                                          
+                    </div>
+                    <div>
+                    <Button variant="primary" size='lg' block onClick={()=>{this.setState(state => ({modal2Open: true}))}}>
+                            Account Settings
+                    </Button>
+                    </div>
+                            
                 </div>
                 <ReactModal isOpen={this.state.modal1Open}
                       onRequestClose={()=>{this.setState(state => ({modal1Open: false}))}}>
-                    <p> Hello I am Modal</p>
+                    <p> Notifications</p>
+                 </ReactModal>
+                 <ReactModal isOpen={this.state.modal2Open}
+                      onRequestClose={()=>{this.setState(state => ({modal2Open: false}))}}>
+                    <p> Account Settings</p>
                  </ReactModal>   
             </div>
         );
