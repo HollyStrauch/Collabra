110c613310bd580ed4018bfadcb55491f1df701b
Holly Strauch <hollystrauch14@gmail.com>
2020-05-23 14:28:43 -0700

    Create Tools Tab Features

--- src/MainScreen/Features/Tools/Tools.js
+++ src/MainScreen/Features/Tools/Tools.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import './Tools.css';
 import RangedDropdown from './RangedDropdown'
-import  Button from 'react-bootstrap/button';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Button from 'react-bootstrap/button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import rec from './record.png'
 
 export default class Files extends React.Component {
     constructor(props) {
         super(props);
-        /* change this to true to make modal appear*/
-        this.state = {dropTitle: 'BPM'};
-      }
+
+        this.state = { checkedMet: false, checkdShare: false };
+    }
 
     render() {
 
         return (
 
-            <div >
-               <RangedDropdown dropTitle="BPM" start={20} range={230} />
+            <div className="Tools-header">
+                <div className="Tools-metronome">
+                    <p>Metronome</p>
+                    <div className="Tools-metSelect">
+                        <RangedDropdown dropTitle="BPM" start={20} range={230} />
+                        <RangedDropdown dropTitle="Beats Per Bar" start={1} range={32} />
+                    </div>
+                    <Switch color="primary" inputProps={{ 'aria-label': 'primary checkbox' }} />
+                    <Button variant="primary" size="small">Reset</Button>
+                </div>
+                <div className="Tools-sharing">
+                    <div className="Tools-metSelect">
+                    Share Metronome
+                        <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
+                    </div>
+                    <div className="Tools-metSelect">
+                        Record
+                        <button style={{ width: '15%' }}><img className="Tools-button" src={rec} alt="record" /></button>
+                    </div>
+
+                </div>
             </div>
         );
     }
 
-
-    
 }
