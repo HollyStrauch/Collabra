8cc02e7aa03f3eeee8d4473c5394abbcdedf157c
Holly Strauch <hollystrauch14@gmail.com>
2020-05-23 14:41:08 -0700

    Add labels

--- src/MainScreen/Features/Tools/Tools.js
+++ src/MainScreen/Features/Tools/Tools.js
@@ -20,20 +20,23 @@ export default class Files extends React.Component {
 
             <div className="Tools-header">
                 <div className="Tools-metronome">
-                    <p>Metronome</p>
-                    <div className="Tools-metSelect">
+                    <h2>Metronome</h2>
+                    <div className = "Tools-metselect">
+                        BPM:
                         <RangedDropdown dropTitle="BPM" start={20} range={230} />
+                        Beats Per Bar:
                         <RangedDropdown dropTitle="Beats Per Bar" start={1} range={32} />
                     </div>
+            
                     <Switch color="primary" inputProps={{ 'aria-label': 'primary checkbox' }} />
                     <Button variant="primary" size="small">Reset</Button>
                 </div>
                 <div className="Tools-sharing">
-                    <div className="Tools-metSelect">
+                    <div className="Tools-inline">
                     Share Metronome
                         <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
                     </div>
-                    <div className="Tools-metSelect">
+                    <div className="Tools-inline">
                         Record
                         <button style={{ width: '15%' }}><img className="Tools-button" src={rec} alt="record" /></button>
                     </div>
