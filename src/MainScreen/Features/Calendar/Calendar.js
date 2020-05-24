b97bc14878ea3f8f8e885c5089fa660918a90c6b
Kercheval-S <kerchevals@cwu.edu>
2020-05-24 16:11:43 -0700

    More functionality
    Buttons added, functions for the button click still need to be coded

--- src/MainScreen/Features/Calendar/Calendar.js
+++ src/MainScreen/Features/Calendar/Calendar.js
@@ -13,21 +13,36 @@ var days = [
     {sunday: 29, monday: 30, tuesday: 31, wednesday: 1, thursday: 2, friday: 3, saturday: 4},
 ];
 
-export default class Calendar extends Component{
+export default class Calendar extends React.Component{
+    constructor(props) {
+        super(props);
+        
+    }
+    apptClick(i) {/**the appointment button clicked */
+
+    }
+    noteClicke(i) {/** the notes button clicked */
+
+    }
+    /**click on a day of the calendar function needed */
 
     render() {
 
         return(
             <div classname="Calendar-header">
-                <div className="date-Selector"/** Picture of the calendar, 6x7 square with a header at the top with month/year */>
+                <div className="date-Selector"/** Picture of the calendar, table with a header at the top with month/year */>
                     <p>Month / Year</p>
                     <CalendarTable data = {day}/>
                 </div>
                 <div className="Appointments-header" /** text box relating to the selected date */>
                     Appointments:
+
+                    <Button variant="primary" size="small">New Appointment</Button>
                 </div>
                 <div className="Notes-header" /** Text box relating to the selected date*/>
                     Notes:
+
+                    <Button variant="primary" size="small">New Note</Button>
                 </div>
             </div>
         );
