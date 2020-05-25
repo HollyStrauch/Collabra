2dc021e2c14c8ab3ccda6af0e0d1f70148e6c1c9
Holly Strauch <hollystrauch14@gmail.com>
2020-05-25 13:06:38 -0700

    Fix filenames

--- src/MainScreen/Features/Calendar/Calendar.js
+++ src/MainScreen/Features/Calendar/Calendar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './calendar.css';
+import './Calendar.css';
 import Button from 'react-bootstrap/button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { render } from '@testing-library/react';
