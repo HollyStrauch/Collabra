728031ab2b0b90e86fb752583fe0d52b3c1caddd
Holly Strauch <hollystrauch14@gmail.com>
2020-05-30 15:08:55 -0700

    Clean up divs

--- src/SideMenu/SideMenu.js
+++ src/SideMenu/SideMenu.js
@@ -5,10 +5,10 @@ http://reactcommunity.org/react-modal/
 
 import React from 'react';
 import './SideMenu.css';
-import './Contacts/contactFeatures.css';
+import './Contacts/ContactFeatures.css';
 import ReactModal from 'react-modal';
-import  Button from 'react-bootstrap/button';
-import {Accordion, Modal} from 'react-bootstrap';
+import Button from 'react-bootstrap/button';
+import { Accordion, Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contacts from './Contacts/Contacts';
 
@@ -16,76 +16,67 @@ import Contacts from './Contacts/Contacts';
 export default class SideMenu extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {modal1Open: false, modal2Open: false, setInCall: props.setInCall};
-      }
+        this.state = { modal1Open: false, modal2Open: false, setInCall: props.setInCall };
+    }
 
     render() {
         return (
-            <div>
-                <div className="SideMenu-header">
-                    <div>                    
-                    <Button  variant="primary" size="lg" block onClick={()=>{this.setState(state => ({modal1Open: true}))}}>
-                            Notifications
-                    </Button>                   
-                    </div>
-                    <div>
-                        <Accordion defaultActiveKey="0" bg="dark">
-                            
-                            <Accordion.Toggle as={Button} variant="success" block size="lg" eventKey="0">
-                                Collaborators
-                            </Accordion.Toggle>
-                                
-                            <Accordion.Collapse eventKey="0">
-                                <div className="contactFeatures-header">
-                                    
-                                
-                                    <Contacts setInCall={this.state.setInCall} inCall={this.props.inCall}/>
-                                
-                                    
-                                </div>
-                            </Accordion.Collapse>
-                        </Accordion>                                          
-                    </div>
-                    <div>
-                    <Button variant="primary" size='lg' block onClick={()=>{this.setState(state => ({modal2Open: true}))}}>
-                            Account Settings
+            <div className="SideMenu-header">
+               
+                    <Button variant="primary" size="lg" block onClick={() => { this.setState(state => ({ modal1Open: true })) }}>
+                        Notifications
                     </Button>
-                    </div>
+                               
+                    <Accordion defaultActiveKey="0" bg="dark">
+                        <Accordion.Toggle as={Button} variant="success" block size="lg" eventKey="0">
+                            Collaborators
+                            </Accordion.Toggle>
+
+                        <Accordion.Collapse eventKey="0">
+                            <div className="contactFeatures-header">
+
+                                <Contacts setInCall={this.state.setInCall} inCall={this.props.inCall} />
+
+                            </div>
+                        </Accordion.Collapse>
+                    </Accordion>
                             
-                </div>
+                    <Button variant="primary" size='lg' block onClick={() => { this.setState(state => ({ modal2Open: true })) }}>
+                        Account Settings
+                    </Button>
                 
                 <ReactModal isOpen={this.state.modal1Open}
-                      onRequestClose={()=>{this.setState(state => ({modal1Open: false}))}}>
-  
-                    <Modal.Header closeButton onClick={()=>{this.setState(state => ({modal1Open: false}))}}>
-                    <Modal.Title>Notifications</Modal.Title>
+                    onRequestClose={() => { this.setState(state => ({ modal1Open: false })) }}>
+
+                    <Modal.Header closeButton onClick={() => { this.setState(state => ({ modal1Open: false })) }}>
+                        <Modal.Title>Notifications</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>Here will be all of the notifications you receive</Modal.Body>
                     <Modal.Footer>
-                        <Button variant="secondary" onClick={()=>{this.setState(state => ({modal1Open: false}))}}>
+                        <Button variant="secondary" onClick={() => { this.setState(state => ({ modal1Open: false })) }}>
                             Close
                         </Button>
-                        <Button variant="primary" onClick={()=>{this.setState(state => ({modal1Open: false}))}}>
+                        <Button variant="primary" onClick={() => { this.setState(state => ({ modal1Open: false })) }}>
                             Save Changes
                         </Button>
                     </Modal.Footer>
-                 </ReactModal>
-                 
-                 <ReactModal isOpen={this.state.modal2Open}
-                      onRequestClose={()=>{this.setState(state => ({modal2Open: false}))}}>
-                    <Modal.Header closeButton onClick={()=>{this.setState(state => ({modal2Open: false}))}}>
-                    <Modal.Title>Account Settings</Modal.Title>
+                </ReactModal>
+
+                <ReactModal isOpen={this.state.modal2Open}
+                    onRequestClose={() => { this.setState(state => ({ modal2Open: false })) }}>
+                    <Modal.Header closeButton onClick={() => { this.setState(state => ({ modal2Open: false })) }}>
+                        <Modal.Title>Account Settings</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>We are going to have all kinds of account settings here</Modal.Body>
                     <Modal.Footer>
-                        <Button variant="secondary" onClick={()=>{this.setState(state => ({modal2Open: false}))}}>
+                        <Button variant="secondary" onClick={() => { this.setState(state => ({ modal2Open: false })) }}>
                             Close
                         </Button>
-                        <Button variant="primary" onClick={()=>{this.setState(state => ({modal2Open: false}))}}>
+                        <Button variant="primary" onClick={() => { this.setState(state => ({ modal2Open: false })) }}>
                             Save Changes
                         </Button>
                     </Modal.Footer>
-                 </ReactModal>   
+                </ReactModal>
             </div>
         );
     }
