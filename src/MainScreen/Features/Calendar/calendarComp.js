9529ea743d9553b10f08567a0daf8bcbb837fc06
Kercheval-S <kerchevals@cwu.edu>
2020-05-31 17:56:37 -0700

    Figuring out dropdown

--- src/MainScreen/Features/Calendar/calendarComp.js
+++ src/MainScreen/Features/Calendar/calendarComp.js
@@ -81,9 +81,18 @@ export default class calendar extends React.Component {
                                     <label>
                                         Appointment start time:
                                         <DropdownButton title="Hour">
-                                            <ul>
-
-                                            </ul>
+                                            <ul>1</ul>
+                                            <ul>2</ul>
+                                            <ul>3</ul>
+                                            <ul>4</ul>
+                                            <ul>5</ul>
+                                            <ul>6</ul>
+                                            <ul>7</ul>
+                                            <ul>8</ul>
+                                            <ul>9</ul>
+                                            <ul>10</ul>
+                                            <ul>11</ul>
+                                            <ul>12</ul>
                                         </DropdownButton>
                                         <DropdownButton title="Minute">
                                             <ul>
